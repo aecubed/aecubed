884d03bcf56ddfc028897f025c45162b4f4b36ad
Emily Bae <emilybae@Emilys-MacBook-Pro-2.local>
2022-07-29 17:12:33 -0700

    setup

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require ('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+require('dotenv').config();
+
+module.exports = {
+	mode: process.env.NODE_ENV,
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+		filename: 'bundle.js',
+    publicPath: '/public'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+          plugins: ['@babel/plugin-transform-runtime', '@babel/transform-async-to-generator'],
+        }
+      },
+      {
+        test: /.(css|scss)$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.png|svg|jpg|gif$/,
+        exclude: /node_modules/,
+        use: ['file-loader'],
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: '/public/index.html',
+      inject: false,
+      minify: false
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'public'),
+      publicPath: '/'
+    },
+    port: 8080,
+    compress: true,
+    hot: true,
+    proxy: { '/**' : {
+      target: 'http://localhost:3000/', 
+      secure: false
+    }}
+  },
+}
