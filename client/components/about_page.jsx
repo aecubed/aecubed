1be4b1a1d4301752e35123051afa4a627f0f406d
awcheung1213 <awcheung1213@gmail.com>
2022-07-31 14:31:40 -0700

    fixed About.jsx

--- client/components/about_page.jsx
+++ client/components/about_page.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 const About = () => {
   return (
-    <div id='about-main'>About
+    <div id='about-container'>About
        <div className='test'>Our Goal</div>
        <div className='test'>Container</div>
        <div className='test'>How We Do It</div>
