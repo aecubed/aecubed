5f162e8f34934cfae737205758002f47ae73aa56
Emily Bae <emilybae@Emilys-MacBook-Pro-2.local>
2022-08-01 12:54:11 -0700

    fixed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'public'),
 		filename: 'bundle.js',
-    publicPath: '/'
+    publicPath: '/public'
   },
   module: {
     rules: [
@@ -73,7 +73,7 @@ module.exports = {
   devServer: {
     static: {
       directory: path.resolve(__dirname, 'public'),
-      publicPath: '/'
+      publicPath: '/public'
     },
     port: 8080,
     compress: true,
