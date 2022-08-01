649ade027983020c53672a21df2003359ef860b4
Eli Muir <eli_muir@Elis-MacBook-Pro.local>
2022-08-01 14:38:50 -0400

    styling tweaks to main page

--- client/components/about_page.jsx
+++ client/components/about_page.jsx
@@ -2,22 +2,30 @@ import React, { useState, useEffect } from 'react';
 
 const About = () => {
   return (
-    <div id='about-container'>
-       <section id='goal'>
+    <div>
+
+       <div id='goal'>
          <div className='goal-container'>Our Goal
            <p className='paragraph1'>Renewable energy provides sustainability as well as increased affordability, and there is a global initiative to expand renewable energy sources. However, they lack consistent energy generation. Our goal is to provide ideal locations for renewable energy expansion to pave the path for a sustainable future.</p>
-         </div>
-       </section>
+         </div>   
+       </div>
+
+       <div>
+
        <section id='how-we-did-it'>
          <div className='how-we-did-it-container'>How We Did It
            <p className='paragraph2'>RLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Facilisis leo vel fringilla est ullamcorper eget nulla facilisi etiam. Nunc lobortis mattis aliquam faucibus purus in massa tempor. Magna eget est lorem ipsum dolor sit amet consectetur. Dui ut ornare lectus sit amet. Gravida neque convallis a cras semper auctor neque. Ornare arcu odio ut sem nulla pharetra diam. Fames ac turpis egestas sed tempus urna et pharetra. Consectetur adipiscing elit duis tristique sollicitudin nibh. Elit duis tristique sollicitudin nibh sit amet commodo nulla facilisi. Congue nisi vitae suscipit tellus mauris a diam maecenas. Nisi est sit amet facilisis. Nisl condimentum id venenatis a. Felis bibendum ut tristique et. Arcu vitae elementum curabitur vitae. Mi eget mauris pharetra et. At volutpat diam ut venenatis tellus in metus vulputate eu. Et odio pellentesque diam volutpat commodo. Habitant morbi tristique senectus et netus et malesuada fames ac. Lectus nulla at volutpat diam ut.</p>
          </div>
        </section>
+
        <section id='more-info'>
          <div className='more-info-container'>More Information
            <p className='paragraph3'>Eu augue ut lectus arcu bibendum. Ipsum a arcu cursus vitae congue. Eu scelerisque felis imperdiet proin fermentum. Phasellus faucibus scelerisque eleifend donec pretium vulputate. Gravida in fermentum et sollicitudin ac orci phasellus egestas. Auctor urna nunc id cursus metus. Viverra justo nec ultrices dui. Nibh venenatis cras sed felis eget velit. Duis tristique sollicitudin nibh sit amet commodo nulla facilisi. Duis at tellus at urna condimentum. Sed felis eget velit aliquet sagittis id consectetur. Lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit. Nibh venenatis cras sed felis. Pellentesque habitant morbi tristique senectus et. Ut diam quam nulla porttitor. Mauris sit amet massa vitae tortor. Arcu odio ut sem nulla pharetra diam sit amet nisl.</p>
          </div>
        </section>
+
+       </div>
+
     </div>
   )
 }
