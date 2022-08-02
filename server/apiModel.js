705f5bb98abec50ed5a9c8fd26394a116593c8d3
Emily Bae <102927506+emilybae1705@users.noreply.github.com>
2022-08-02 10:19:42 -0500

    Update apiModel.js

--- server/apiModel.js
+++ server/apiModel.js
@@ -11,10 +11,6 @@ mongoose
 
 const Schema = mongoose.Schema;
 
-/* terminal script to store json data to mongodb collection:
-mongoimport --uri="mongodb+srv://emilybae1705:jDBx91fopPtomCJD@cluster0.dckxv74.mongodb.net/?retryWrites=true&w=majority" 
---jsonArray --file=dump/weatherData.json
-*/
 const specificSchema = new Schema({
   median: Number,
   mean: Number,
