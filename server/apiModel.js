b7167cea6d6db57033202a374da5dd7e301636ac
Eli Muir <eli_muir@Elis-MacBook-Pro.local>
2022-08-03 16:08:23 -0400

    weather.getFips works

--- server/apiModel.js
+++ server/apiModel.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const { MONGO_URI } = require('./secrets.js')
 
+
 mongoose
   .connect(MONGO_URI, {
     useNewUrlParser: true,
--- server/routes/map.js
+++ server/routes/map.js
@@ -3,6 +3,9 @@ const express = require('express');
 const apiController = require('../apiController');
 const mapRouter = express.Router();
 
+const weather = require('../apiController.js')
+console.log(weather)
+
 // mapRouter.post('/', 
 //   apiController.directGeocode, apiController.getWeatherData, 
 //   apiController.saveData, apiController.queryData, apiController.comparedDetails,
@@ -21,7 +24,7 @@ mapRouter.get('/states', (req, res)=> {
 })
 
 // fire after selecting state
-mapRouter.get('/states/:state', (req, res)=> { // to get counties - look into how to make router end point url more intuitive
+mapRouter.get('/states/:state',  (req, res)=> { // to get counties - look into how to make router end point url more intuitive
   //recieves user input state in req.params.state 
   //returns the counties 
   return res.status(200).json();
@@ -30,11 +33,11 @@ mapRouter.get('/states/:state', (req, res)=> { // to get counties - look into ho
 // fire after clicking the submit button
 
 //
-mapRouter.get('/states/:state/:county', (req, res)=> { // to get weather
+mapRouter.get('/states/:state/:county', weather.getFips,  (req, res)=> { // to get weather // test later  
   //recieve user input county in req.params.county
   //req.params.state ->new york 
   //const {state, county} = req.params; > fetch data API with state + county > > 
-  return res.status(200).json(req.params);
+  return res.status(200).json(res.locals.data);
   // query api by using fips as params here
   // handle data 
 });
